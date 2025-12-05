8a249c6418fb834cc1888e49adf2fef55bb17311
Joaquin <joaquin1713.jg@gmail.com>
2025-12-05 17:20:17 -0300

    pequeños cambios, falta lo de las imagenes

--- wwwroot/js/js.js
+++ wwwroot/js/js.js
@@ -1,8 +1,37 @@
-window.alert = function (){ 
+window.guardar = function (){ 
 
     Swal.fire({
-        title: "Good job!",
-        text: "You clicked the button!",
+        title: "Bien Hecho!",
+        text: "Se ha guardado con exito",
         icon: "success"
+    });
+}
+
+window.eliminar = function () {
+    return Swal.fire({
+        title: "Estas segura?",
+        text: "No puedes revertir esto!",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#3085d6",
+        cancelButtonColor: "#d33",
+        confirmButtonText: "Si, Eliminar!"
+    }).then((result) => {
+        if (result.isConfirmed) {
+            Swal.fire({
+                title: "Eliminado!",
+                text: "El producto fue eliminado.",
+                icon: "success"
+            });
+        }
+        return result.isConfirmed;
+    });
+}
+
+window.alerta = function () {
+    Swal.fire({
+        title: "Alerta",
+        text: "El producto no se sumo por falta de stock!",
+        icon: "info"
     });
 }
